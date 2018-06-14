88929b4b61fa717da10aac9d70521ae1c248ac5c
Den <35501258+denver411@users.noreply.github.com>
2018-06-14 20:10:45 +0300

    Add files via upload

---
+++ exelente_ru/js/school-page.js
@@ -0,0 +1,15 @@
+'use strict'
+let programItemList = document.querySelectorAll('.programs__list-item');
+let programItemInfo = document.querySelectorAll('.programs__info-item');
+console.log(programItemList, programItemInfo);
+
+programItemList.forEach(function (item, index){
+  item.onclick = function() {
+    let activeItemList = document.querySelector('.programs__list-item_active');
+    let activeItemInfo = document.querySelector('.programs__info-item_active');
+    activeItemList.classList.remove('programs__list-item_active');
+    this.classList.add('programs__list-item_active')
+    activeItemInfo.classList.remove('programs__info-item_active');
+    programItemInfo[index].classList.add('programs__info-item_active')
+  }
+})
