e9222df3ac278917a4e2ebc21a240da53b03fcc4
Denis Zhebov <zhebov@gmail.com>
2018-06-26 07:29:57 +0300

    Create password-restore-1.js

---
+++ exelenteRu/js/password-restore-1.js
@@ -0,0 +1,15 @@
+//Проверка поля почта
+
+let restore-button_1 = document.querySelector('button button-action__reset-password');
+restore-button_1.onclick = function () {
+    let checkMailInput = document.querySelector('.e-mail__field').value;
+
+    if (!(/^[\w\d]+[\-\_\.\w\d]+\@+[\w\d]+\.[\w]+$/).test(checkMailInput) || !checkMailInput) {
+        document.querySelector('.e-mail__field').style.background = '#c82e2e24';
+        // document.querySelector('.e-mail__field').style.display = 'block';
+        return false;
+    } else {
+        document.querySelector('.e-mail__field').style.background = '#5ac82e24';
+       //document.querySelector('.e-mail__field').value = 'none';
+    }
+}
