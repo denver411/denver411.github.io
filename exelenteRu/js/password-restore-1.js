1fa7a8aa054c4ae9a5a75d07f6c48d16f4f8c4bd
Denis Zhebov <zhebov@gmail.com>
2018-06-26 07:37:18 +0300

    Update password-restore-1.js

--- exelenteRu/js/password-restore-1.js
+++ exelenteRu/js/password-restore-1.js
@@ -3,13 +3,16 @@
 let restore-button_1 = document.querySelector('.button-action__reset-password');
 restore-button_1.onclick = function () {
     let checkMailInput = document.querySelector('.e-mail__field').value;
+    let checkMailInputLink = document.querySelector('.e-mail__field > a');
 
     if (!(/^[\w\d]+[\-\_\.\w\d]+\@+[\w\d]+\.[\w]+$/).test(checkMailInput) || !checkMailInput) {
         document.querySelector('.e-mail__field').style.background = '#c82e2e24';
         // document.querySelector('.e-mail__field').style.display = 'block';
+        checkMailInputLink.href = null;
         return false;
     } else {
         document.querySelector('.e-mail__field').style.background = '#5ac82e24';
-       //document.querySelector('.e-mail__field').value = 'none';
+        checkMailInputLink.href = 'password-restore-2-step.html';
+        //document.querySelector('.e-mail__field').value = 'none';
     }
 }
