95323106141de1a0c288092d48cef7662c4f3341
Denis Zhebov <zhebov@gmail.com>
2018-06-27 07:46:37 +0300

    Update blog.js

--- exelenteRu/blocks/blog/blog.js
+++ exelenteRu/blocks/blog/blog.js
@@ -3,17 +3,8 @@ let blogMenu = document.querySelectorAll('.blog__menu-item');
 let blogNews = document.querySelectorAll('.blog__news-list');
 console.log(blogMenu);
 
-/*blogMenu.forEach(function (item, index) {
-  item.onclick = function () {
-    let activeBlogMenu = document.querySelector('.blog__menu-item_active');
-    let activeBlogNews = document.querySelector('.blog__news-list_active');
-    activeBlogMenu.classList.remove('blog__menu-item_active');
-    this.classList.add('blog__menu-item_active')
-    activeBlogNews.classList.remove('blog__news-list_active');
-    blogNews[index].classList.add('blog__news-list_active')
-  }
-})*/
 
+if (navigator.userAgent.search(/MSIE/) > 0 || navigator.userAgent.search(/NET CLR /) > 0) {
 for (let i = 0; i <= blogMenu.length; i++)  {
   blogMenu[i].onclick = function () {
     let activeBlogMenu = document.querySelector('.blog__menu-item_active');
@@ -23,3 +14,15 @@ for (let i = 0; i <= blogMenu.length; i++)  {
     activeBlogNews.classList.remove('blog__news-list_active');
     blogNews[i].classList.add('blog__news-list_active')
   }
+} else {
+  blogMenu.forEach(function (item, index) {
+  item.onclick = function () {
+    let activeBlogMenu = document.querySelector('.blog__menu-item_active');
+    let activeBlogNews = document.querySelector('.blog__news-list_active');
+    activeBlogMenu.classList.remove('blog__menu-item_active');
+    this.classList.add('blog__menu-item_active')
+    activeBlogNews.classList.remove('blog__news-list_active');
+    blogNews[index].classList.add('blog__news-list_active')
+  }
+})
+}
