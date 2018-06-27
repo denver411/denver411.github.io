c715216cce01c57d00acb479cd39f695e25ec4e6
Denis Zhebov <zhebov@gmail.com>
2018-06-27 07:48:58 +0300

    Update blog.js

--- exelenteRu/blocks/blog/blog.js
+++ exelenteRu/blocks/blog/blog.js
@@ -5,24 +5,25 @@ console.log(blogMenu);
 
 
 if (navigator.userAgent.search(/MSIE/) > 0 || navigator.userAgent.search(/NET CLR /) > 0) {
-for (let i = 0; i <= blogMenu.length; i++)  {
-  blogMenu[i].onclick = function () {
-    let activeBlogMenu = document.querySelector('.blog__menu-item_active');
-    let activeBlogNews = document.querySelector('.blog__news-list_active');
-    activeBlogMenu.classList.remove('blog__menu-item_active');
-    this.classList.add('blog__menu-item_active')
-    activeBlogNews.classList.remove('blog__news-list_active');
-    blogNews[i].classList.add('blog__news-list_active')
-  }
+  for (let i = 0; i <= blogMenu.length; i++)  {
+    blogMenu[i].onclick = function () {
+      let activeBlogMenu = document.querySelector('.blog__menu-item_active');
+      let activeBlogNews = document.querySelector('.blog__news-list_active');
+      activeBlogMenu.classList.remove('blog__menu-item_active');
+      this.classList.add('blog__menu-item_active')
+      activeBlogNews.classList.remove('blog__news-list_active');
+      blogNews[i].classList.add('blog__news-list_active')
+    }
+  } 
 } else {
   blogMenu.forEach(function (item, index) {
-  item.onclick = function () {
-    let activeBlogMenu = document.querySelector('.blog__menu-item_active');
-    let activeBlogNews = document.querySelector('.blog__news-list_active');
-    activeBlogMenu.classList.remove('blog__menu-item_active');
-    this.classList.add('blog__menu-item_active')
-    activeBlogNews.classList.remove('blog__news-list_active');
-    blogNews[index].classList.add('blog__news-list_active')
-  }
-})
+    item.onclick = function () {
+      let activeBlogMenu = document.querySelector('.blog__menu-item_active');
+      let activeBlogNews = document.querySelector('.blog__news-list_active');
+      activeBlogMenu.classList.remove('blog__menu-item_active');
+      this.classList.add('blog__menu-item_active')
+      activeBlogNews.classList.remove('blog__news-list_active');
+      blogNews[index].classList.add('blog__news-list_active')
+    }
+  })
 }
