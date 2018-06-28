ab004193ee61eff83af23713e34afe1036c5c153
Denis Zhebov <zhebov@gmail.com>
2018-06-28 07:48:31 +0300

    Add files via upload

--- exelenteRu/blocks/blog/blog.js
+++ exelenteRu/blocks/blog/blog.js
@@ -1,14 +1,17 @@
 'use strict'
 let blogMenu = document.querySelectorAll('.blog__menu-item');
 let blogNews = document.querySelectorAll('.blog__news-list');
-blogMenu.forEach = Array.prototype.forEach;
-  blogMenu.forEach(function (item, index) {
-    item.onclick = function () {
-      let activeBlogMenu = document.querySelector('.blog__menu-item_active');
-      let activeBlogNews = document.querySelector('.blog__news-list_active');
-      activeBlogMenu.classList.remove('blog__menu-item_active');
-      this.classList.add('blog__menu-item_active')
-      activeBlogNews.classList.remove('blog__news-list_active');
-      blogNews[index].classList.add('blog__news-list_active')
-    }
-  })
+if (navigator.userAgent.search(/MSIE/) > 0 || navigator.userAgent.search(/NET CLR /) > 0) {
+  blogMenu.forEach = Array.prototype.forEach;
+}
+
+blogMenu.forEach(function (item, index) {
+  item.onclick = function () {
+    let activeBlogMenu = document.querySelector('.blog__menu-item_active');
+    let activeBlogNews = document.querySelector('.blog__news-list_active');
+    activeBlogMenu.classList.remove('blog__menu-item_active');
+    this.classList.add('blog__menu-item_active')
+    activeBlogNews.classList.remove('blog__news-list_active');
+    blogNews[index].classList.add('blog__news-list_active')
+  }
+})
