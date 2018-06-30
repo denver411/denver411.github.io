1a80d0e9a069e0190c7dc64ce98c2a82d63d0cc6
Denis Zhebov <zhebov@gmail.com>
2018-06-30 10:57:11 +0300

    Add files via upload

--- exelenteRu/blocks/blog/blog.js
+++ exelenteRu/blocks/blog/blog.js
@@ -1,9 +1,11 @@
 'use strict'
-let blogMenu = document.querySelectorAll('.blog__menu-item');
-let blogNews = document.querySelectorAll('.blog__news-list');
-if (navigator.userAgent.search(/MSIE/) > 0 || navigator.userAgent.search(/NET CLR /) > 0) {
-  blogMenu.forEach = Array.prototype.forEach;
-}
+
+// переключение по заголовкам
+let blogMenu = Array.from(document.querySelectorAll('.blog__menu-item'));
+let blogNews = Array.from(document.querySelectorAll('.blog__news-list'));
+// if (navigator.userAgent.search(/MSIE/) > 0 || navigator.userAgent.search(/NET CLR /) > 0) {
+//   blogMenu.forEach = Array.prototype.forEach;
+// }
 
 blogMenu.forEach(function (item, index) {
   item.onclick = function () {
@@ -15,3 +17,39 @@ blogMenu.forEach(function (item, index) {
     blogNews[index].classList.add('blog__news-list_active')
   }
 })
+
+// переключение по кнопкам навигации
+
+let blogClickNext = document.querySelector('.blog__navigation .navigation__arrow_right');
+let blogClickPrev = document.querySelector('.blog__navigation .navigation__arrow_left');
+let blogPosition = 0;
+
+blogClickNext.onclick = function () {
+  blogPosition = blogMenu.indexOf(document.querySelector('.blog__menu-item_active'));
+  
+  blogMenu[blogPosition].classList.remove('blog__menu-item_active');
+  blogNews[blogPosition].classList.remove('blog__news-list_active');
+
+  blogPosition++;
+  if (blogPosition >= blogMenu.length) {
+    blogPosition = 0;
+  }
+  
+  blogMenu[blogPosition].classList.add('blog__menu-item_active');
+  blogNews[blogPosition].classList.add('blog__news-list_active');
+}
+
+blogClickPrev.onclick = function () {
+  blogPosition = blogMenu.indexOf(document.querySelector('.blog__menu-item_active'));
+  
+  blogMenu[blogPosition].classList.remove('blog__menu-item_active');
+  blogNews[blogPosition].classList.remove('blog__news-list_active');
+
+  blogPosition--;
+  if (blogPosition < 0) {
+    blogPosition = blogMenu.length - 1;
+  }
+  
+  blogMenu[blogPosition].classList.add('blog__menu-item_active');
+  blogNews[blogPosition].classList.add('blog__news-list_active');
+}
